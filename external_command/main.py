7841a80c8bc71e92f079802ea111a71bdae80d7f
Alejandro Guirao <lekumberri@gmail.com>
2016-06-22 08:53:39 +0200

    Add example with subprocess.Popen

--- external_command/main.py
+++ external_command/main.py
@@ -16,9 +16,32 @@ def execute_command(command):
         stderr = e.output.decode()
     return return_code, stdout, stderr
 
+def execute_command_2(command):
+    """
+    Execute command and return a tuple with the return code, the stdout and the stderr
+    """
+
+    p = subprocess.Popen(command.strip(),
+                         stdout= subprocess.PIPE,
+                         stderr= subprocess.PIPE)
+
+    out, err = p.communicate()
+    stdout = out.decode('utf-8')
+    stderr = err.decode('utf-8')
+    return_code = p.returncode
+    return return_code, stdout, stderr
+
 
 if __name__ == '__main__':
     c, o, e = execute_command("ls -lrt")
-    print("Return code:", c, "Stdout:", o, "Stderr", e)
+    print("Return code:", c, "Stdout:", o, "Stderr:", e)
+    print()
     c, o, e = execute_command("cat sdfdkjfkddf")
-    print("Return code:", c, "Stdout:", o, "Stderr", e)
+    print("Return code:", c, "Stdout:", o, "Stderr:", e)
+    print()
+
+    c, o, e = execute_command("ls -lrt")
+    print("Return code:", c, "Stdout:", o, "Stderr:", e)
+    print()
+    c, o, e = execute_command("cat sdfdkjfkddf")
+    print("Return code:", c, "Stdout:", o, "Stderr:", e)
