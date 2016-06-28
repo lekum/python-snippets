d8d71efe8acfc90e1ec440021264b288fba278b1
Alejandro Guirao <lekumberri@gmail.com>
2016-06-28 09:45:59 +0200

    Add example of pytest monkeypatch

--- pytest-fixtures/fixtures.py
+++ pytest-fixtures/fixtures.py
@@ -1,5 +1,7 @@
 import pytest
+import requests
 import random
+from unittest.mock import MagicMock
 
 @pytest.fixture
 def random_word():
@@ -9,3 +11,9 @@ def random_word():
 def test_upper(random_word):
     assert random_word.upper() != random_word
 
+def test_example_requests(monkeypatch):
+    mockreturn = MagicMock(return_value="kk")
+    monkeypatch.setattr(requests, 'get', mockreturn)
+    r = requests.get("http://myurl.com")
+    assert r == "kk"
+    mockreturn.assert_called_once_with("http://myurl.com")
