7ceaaea67542fbc13d1858d6f05deae053e7c207
Alejandro Guirao <lekumberri@gmail.com>
2016-06-29 09:48:03 +0200

    Add example of mock with side_effect

--- pytest-fixtures/fixtures.py
+++ pytest-fixtures/fixtures.py
@@ -1,7 +1,7 @@
 import pytest
 import requests
 import random
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, call
 
 @pytest.fixture
 def random_word():
@@ -12,8 +12,11 @@ def test_upper(random_word):
     assert random_word.upper() != random_word
 
 def test_example_requests(monkeypatch):
-    mockreturn = MagicMock(return_value="kk")
-    monkeypatch.setattr(requests, 'get', mockreturn)
-    r = requests.get("http://myurl.com")
-    assert r == "kk"
-    mockreturn.assert_called_once_with("http://myurl.com")
+    mockrequests = MagicMock(side_effect=[0, 1, 2])
+    monkeypatch.setattr(requests, 'get', mockrequests)
+    responses = []
+    for i in range(3):
+        responses.append(requests.get("http://myurl.com/%d" % i))
+
+    assert responses == [0, 1, 2]
+    assert mockrequests.call_args_list == [call("http://myurl.com/0"), call("http://myurl.com/1"), call("http://myurl.com/2")]
