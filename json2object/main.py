f9225455e98bfc33ef45eddce86ce495c44a4fee
Alejandro Guirao <lekumberri@gmail.com>
2016-07-07 09:37:20 +0200

    Add example of JSON serialization / deserialization

--- json2object/main.py
+++ json2object/main.py
@@ -8,6 +8,32 @@ class JSONObject:
 def json2object(j):
     return json.loads(j, object_hook=JSONObject)
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def serialize_instance(obj):
+    d = {'__classname__': type(obj).__name__ }
+    d.update(vars(obj))
+    return d
+
+classes = {
+        'Point': Point
+        }
+
+def unserialize_object(d):
+    clsname = d.pop('__classname__', None)
+    if clsname:
+        cls = classes[clsname]
+        obj = cls.__new__(cls) # Make instance without calling __init__
+        for key, value in d.items():
+            setattr(obj, key, value)
+        return obj
+    else:
+        return d
+
+
 if __name__ == '__main__':
 
     d = '{"name": "ACME", "shares": 50, "price": 490.1}'
@@ -15,3 +41,9 @@ if __name__ == '__main__':
     print(d1.name)
     print(d1.shares)
     print(d1.price)
+    p = Point(2, 3)
+    s = json.dumps(p, default=serialize_instance)
+    print(s)
+    a = json.loads(s, object_hook=unserialize_object)
+    print(a.x)
+    print(a.y)
