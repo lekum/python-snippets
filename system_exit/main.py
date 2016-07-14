8b9f5d5acb91d3725416f4f603d88b4a473a0ef0
Alejandro Guirao <lekumberri@gmail.com>
2016-07-14 08:59:53 +0200

    Add example of getpass

--- system_exit/main.py
+++ system_exit/main.py
@@ -1,7 +1,11 @@
 import sys
+import getpass
 
 if __name__ == '__main__':
 
     if len(sys.argv) < 2:
         raise SystemExit("Usage: python main.py arg1 arg2 ...")
     print("Everything right!")
+    user = getpass.getuser()
+    passwd = getpass.getpass()
+    print("User:", user, "Password:", passwd)
