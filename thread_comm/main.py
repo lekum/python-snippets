03d39ddb06c4726bfcb17df49a5c621d9ddb7e68
Alejandro Guirao <lekumberri@gmail.com>
2016-08-24 08:45:10 +0200

    Change for a sentinel value

--- thread_comm/main.py
+++ thread_comm/main.py
@@ -1,16 +1,18 @@
 from queue import Queue
 from threading import Thread
 
+_sentinel = object()
+
 def producer(out_q):
     for i in range(10):
         out_q.put(i)
-    out_q.put("end")
+    out_q.put(_sentinel)
 
 def consumer(in_q):
     while True:
         data = in_q.get()
-        if data == "end":
-            return
+        if data is _sentinel:
+            break
         print("I got {}".format(data))
 
 if __name__ == "__main__":
