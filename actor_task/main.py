dd4d5c14c3d4ad33d2920b47dcbd0395072d2fa4
Alejandro Guirao <lekumberri@gmail.com>
2016-09-02 09:30:46 +0200

    Improve actor with a subclass

--- actor_task/main.py
+++ actor_task/main.py
@@ -6,6 +6,21 @@ class PrintActor(Actor):
             msg = self.recv()
             print("Got:", msg)
 
+
+class TaggedActor(Actor):
+
+    def run(self):
+        while True:
+            tag, *payload = self.recv()
+            getattr(self,'do_'+tag)(*payload)
+
+    # Methods correponding to different message tags
+    def do_A(self, x):
+        print('Running A', x)
+
+    def do_B(self, x, y):
+        print('Running B', x, y)
+
 if __name__ == "__main__":
 
     p = PrintActor()
@@ -14,3 +29,10 @@ if __name__ == "__main__":
     p.send("World!")
     p.close()
     p.join()
+
+    t = TaggedActor()
+    t.start()
+    t.send(("A", "hello"))
+    t.send(("B", "hello", "world!"))
+    t.close()
+    t.join()
