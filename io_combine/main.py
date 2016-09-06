9444caff5cb13b5058d74361b3c57cc114ed0a9f
Alejandro Guirao <lekumberri@gmail.com>
2016-09-06 11:19:41 +0200

    Add io combine with generator function

---
+++ io_combine/main.py
@@ -0,0 +1,16 @@
+sample = "En un lugar de la mancha de cuyo nombre no quiero acordarme".split()
+
+def combine(source, maxsize):
+    parts = []
+    size = 0
+    for part in source:
+        parts.append(part)
+        size += len(part)
+        if size > maxsize:
+            yield ''.join(parts)
+            parts = []
+            size = 0
+    yield ''.join(parts)
+
+for part in combine(sample, 10):
+    print(part)
